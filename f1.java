5e3610f23e317bb5461cc90c6a8da155cfcab63a
Jaya-prakash05 <mjayaprakash736@gmail.com>
2026-08-07 13:16:37 +0530

    3 files are modified third time in master

--- f1.java
+++ f1.java
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
+
 first modification in f1.java in third
 second modification in f1.java in master
-=======
+
 first modification in f1.java in second
 second modification in f1.java in second
->>>>>>> second
+
+third modification in f1.java in master
--- f2.java
+++ f2.java
@@ -1,2 +1,3 @@
 first modification in f2.java in third
 second modification in f2.java in master
+third modification in f2.java in master
